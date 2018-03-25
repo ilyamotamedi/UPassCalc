eafb42a7648fdc1e50d66cdafaad30f889461eb4
Ilya Motamedi <ilya.motamedi@gmail.com>
2018-03-25 02:38:10 -0400

    Initial commit

---
+++ js/dist.js
@@ -0,0 +1,47 @@
+const uPass = 282.5,
+      metropass = 116.75;
+
+var input = {
+    daysPerWeek : '',
+    faresPerDay : '',
+    goTransit : '',
+    prestoCard : '',
+    ageRange : '',
+    weeksPerSemester : ''
+};
+
+var currentCost = {
+    singleFare: '',
+    metropass: ''
+};
+
+var calculate = () => {
+    $.validate({lang: 'en'});
+    input.daysPerWeek = Number($('#daysPerWeek').val());
+    input.faresPerDay = Number($('#faresPerDay').val());
+    input.goTransit = Number($('#goTransit').val());
+    input.prestoCard = Number($('#prestoCard').val());
+    input.ageRange = Number($('#ageRange').val());
+    input.weeksPerSemester = Number($('#weeksPerSemester').val());
+    
+    // console.log(input);
+    // console.log(((input.ageRange == 0 ? (input.prestoCard == 1 ? 2.05 : 2.1) : (input.prestoCard == 1 ? 3 : 3.25)) - input.goTransit));
+    
+    currentCost.singleFare = Math.round((input.daysPerWeek * input.faresPerDay * ((input.ageRange == 0 ? (input.prestoCard == 1 ? 2.05 : 2.1) : (input.prestoCard == 1 ? 3 : 3.25)) - input.goTransit)) * input.weeksPerSemester, 2);
+    currentCost.metropass = input.eeksPerSemester > 12 ? metropass * 4 : (input.weeksPerSemester > 8 ? metropass * 3 : (input.weeksPerSemester > 4 ? metropass * 2 : metropass));
+    console.log(currentCost.singleFare, currentCost.metropass);
+
+    $('#currentSingleFare').html(currentCost.singleFare);
+    $('#resultSingleFare').html(currentCost.singleFare > uPass ? 'save' : 'lose');
+    
+    $('#currentMetropass').html(currentCost.metropass);
+    $('#resultMetropass').html(currentCost.metropass > uPass ? 'save' : 'lose');    
+};
+
+
+$(document).ready(() => {
+    calculate();
+    $('#calculate').click(() => {
+        calculate();
+    });
+});
